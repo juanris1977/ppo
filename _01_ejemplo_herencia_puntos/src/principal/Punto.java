322149d6bc0d1361e71ca36e8ee3708864847379
juanris1977 <120179191+juanris1977@users.noreply.github.com>
2023-01-09 09:25:09 +0100

    inicio ppo

--- _01_ejemplo_herencia_puntos/src/principal/Punto.java
+++ _01_ejemplo_herencia_puntos/src/principal/Punto.java
@@ -1,5 +1,31 @@
 package principal;
 
 public class Punto {
+	private int x;
+	private int y;
+	//inicializa atributos
+	public Punto(int x, int y ) {
+		this.x=x;
+		this.y=y;
+	}
+	public Punto() {
+		x= 10;
+		y= 5;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	//settet and getter
+	
 
 }
