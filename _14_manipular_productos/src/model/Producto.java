26aafc79554259fcb64dc37f2848219f014cb6cd
juanra02p <juanra02p@gmail.com>
2023-01-18 22:08:22 +0100

    18 enero casa ejercicios hechos

--- _14_manipular_productos/src/model/Producto.java
+++ _14_manipular_productos/src/model/Producto.java
@@ -4,39 +4,29 @@ public class Producto {
 	private String nombre_producto;
 	private double precio;
 	private String categoria;
-	
 	public Producto(String nombre_producto, double precio, String categoria) {
 		super();
 		this.nombre_producto = nombre_producto;
 		this.precio = precio;
 		this.categoria = categoria;
 	}
-
 	public String getNombre_producto() {
 		return nombre_producto;
 	}
-
 	public void setNombre_producto(String nombre_producto) {
 		this.nombre_producto = nombre_producto;
 	}
-
 	public double getPrecio() {
 		return precio;
 	}
-
 	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
-
 	public String getCategoria() {
 		return categoria;
 	}
-
-	public void setCategoria(String categoria) {
-		this.categoria = categoria;
+	public void setCartegoria(String cartegoria) {
+		this.categoria = cartegoria;
 	}
-	
-	
-	
 
 }
--- _16_Pruebas_streams/src/principal/test2.java
+++ _16_Pruebas_streams/src/principal/test2.java
@@ -8,6 +8,7 @@ public class test2 {
 		List <Integer> nums = List.of(6,11,-4,-5,6,10,6,8,2,11,27,-5,-12,41,10);
 		
 		//Muestra los 7 primeros no duplicados
+		System.out.println("Muestra los 7 primeros no duplicados:");
 		
 		nums.stream()
 		.distinct()
@@ -15,7 +16,14 @@ public class test2 {
 		.forEach(n -> System.out.println(n));						  
 
 		// hay algun negativo ?
+		System.out.println("Hay negativos?");
+		System.out.println(nums.stream()
+		                   .anyMatch(n -> n<0));
+		
 		
 		//  son todos pares ??
+		System.out.println("Todos pares: ");
+		System.out.println(nums.stream()
+					       .allMatch(t -> t%2==0));
 	}
 }
